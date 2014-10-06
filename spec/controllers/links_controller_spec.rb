2b0d10c8cc809dc9f2519a9dba078c81da063767
Robert Hernandez <r.hernandez@berkeley.edu>
2014-10-06 00:24:59 -0700

    preimary test

--- spec/controllers/links_controller_spec.rb
+++ spec/controllers/links_controller_spec.rb
@@ -1,6 +1,17 @@
 require 'spec_helper'
+require 'rails_helper'
 
 describe LinksController do
-  describe 'trying an invalid link' do
+  describe 'POST #create' do
+  	context 'when url is invalid' do
+	    it 'renders the page with error' do
+	      url = create(:link)
+
+	      post :create, session: { link: url.url}
+
+	      expect(response).to render_template(:index)
+	      expect(flash[:notice]).to match(/^ do not match/)
+	    end
+	end
   end
 end
---
+++ spec/features/user_creates_a_minilink_spec.rb
@@ -0,0 +1,12 @@
+# spec/features/user_creates_a_minilink_spec.rb
+
+feature 'User creates a Small link' do
+  scenario 'they use a valid url' do
+    visit root_path
+
+    #fill_in 'Name', with: 'My foobar'
+    #click_button 'Create Foobar'
+
+    #expect(page).to have_css '.foobar-name', 'My foobar'
+  end
+end
